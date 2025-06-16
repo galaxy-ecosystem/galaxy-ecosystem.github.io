6de880807aaddd9b04d0bdab35aca527dc4c43da
ryanr549 <ryanr549@github.com>
2025-06-16 13:41:08 +0800

    mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: "https://galaxy-ecosystem.github.io",
-  base: "/",
   vite: {
     plugins: [tailwindcss()]
   },
